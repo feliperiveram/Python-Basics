ea809e5e39b6f58ad17841034efd3643ffd49cfe
feliperiveram <134973157+feliperiveram@users.noreply.github.com>
2023-06-05 18:41:48 -0400

    Clase 05.06

---
+++ Experiencia 3/05.06 unidimensionales.py
@@ -0,0 +1,31 @@
+import numpy as np
+lista = [1,2,3,4,5]
+arreglo = np.array(lista)
+print(arreglo)
+
+#ndim = número de dimensiones
+print(arreglo.ndim)
+
+#len = largo del arreglo
+print(len(arreglo))
+
+#slice -> nombre_arreglo[desde donde : hasta donde sin tomarlo en cuenta : step]
+print(arreglo[1:3:1])
+print(arreglo[:3:])
+
+#crear arreglos
+lista2 = [i for i in range(1,11)]
+arreglo2 = np.array(lista2)
+print(arreglo2)
+
+print(np.arange(1,11,2))
+
+#sumar
+arreglo += 5
+print (arreglo)
+
+#suma de todos los valores
+print (arreglo.sum())
+
+#promedio de los valores
+print (arreglo.mean())
---
+++ Experiencia 3/05.06.py bidimensionales.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+lista = [[1,2,3],[4,5,6],[7,8,9]]
+arreglo = np.array(lista)
+print(arreglo)
+
+#nombre_arreglo[vertical][horizontal]
+print(arreglo[1][1])
+print(arreglo[-3][-3])
+
+arr = np.arange(1,101).reshape(10,10)
+print(arr)
+
+#mostrar todos los valores de la matriz
+for x in range(10):
+    for y in range(10):
+        print(arr[x][y])
+
+for y in range(10):
+    for x in range(10):
+        if (arr[y][x]) == 55:
+            print(arr[y][x])
+
+print(arreglo.ndim)
+
+#slice -> (nombre_arreglo[::,::])
+
+print(arr[3:6:,:5:])
